582949ee5eee1611ff755a22a73c4b4704cbb079
thomashaaland <thomas.haaland@gmail.com>
2021-03-06 19:07:16 +0100

    Recursive functional

--- variadic/variadic.cpp
+++ variadic/variadic.cpp
@@ -1,12 +1,22 @@
+#include <functional>
+#include <iostream>
 #include <cstdio>
 
-int main() {
-  char buffer [7];
+void print() {
+  std::cout << "Empty function!" << "\n";
+}
 
-  int n;
-  
-  n = sprintf(buffer, "Hello\n");
-  printf("%s", buffer);
+template <typename T, typename... Types>
+void print(T var1, Types... var2)
+{
+  std::cout << var1 << "\n";
+
+  print(var2...);
+}
+
+int main() {
+
+  print("Hello", 1, 2, 3, "the end!");
   
   return 0;
 }
