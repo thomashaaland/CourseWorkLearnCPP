c657d0db8cbfb680929c15be8c51d4632c7eadef
thomashaaland <thomas.haaland@gmail.com>
2021-04-05 20:55:36 +0200

    unique_ptr

--- smart_pointer/main.cpp
+++ smart_pointer/main.cpp
@@ -1,8 +1,16 @@
 #include "Sclass.h"
+#include <memory>
+#include <cassert>
+
+std::unique_ptr<Sclass> pass_through(std::unique_ptr<Sclass> p) {
+  return p;
+}
 
 int main() {
-  Sclass s;
-  
 
+  auto s = std::make_unique<Sclass>();
+  auto ss = pass_through(std::move(s));
+  assert(!s);
+  
   return 0;
 }
